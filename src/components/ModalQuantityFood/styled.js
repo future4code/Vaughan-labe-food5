56fcce828f7ac24e5c6aa59ba95309685c2dbdb5
Raquelmms <raqueldemedeirosmartins@gmail.com>
2022-03-15 14:02:03 -0300

    modal das quantidades dos produtos

---
+++ src/components/ModalQuantityFood/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+
+export const CtnMargin = styled.div`
+margin-top: 2rem;
+margin-bottom: 2rem;
+`
+
+export const CtnCloseBtn = styled.div`
+display:flex;
+justify-content:flex-end;
+margin-bottom:1rem;
+`
