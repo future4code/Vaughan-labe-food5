77ef7db1501e504f52c8a0ae19c498acbf403783
Raquelmms <raqueldemedeirosmartins@gmail.com>
2022-03-16 10:49:52 -0300

    modal funcionando com bg transparente

--- src/pages/restaurantpage/RestaurantPage.js
+++ src/pages/restaurantpage/RestaurantPage.js
@@ -5,54 +5,76 @@ import { BASE_URL } from "../../constants/urls";
 import { useRequestData } from "../../hooks/useRequestData";
 import { useParams } from "react-router-dom";
 //import {useProtectedPage} from "../../hooks/useProtectedPage"
-import {GlobalStateContext} from "../../components/Global/GlobalStateContext";
+import { GlobalStateContext } from "../../components/Global/GlobalStateContext";
 import { useContext } from "react";
-import Footer from '../../components/Footer/Footer'
+import Footer from "../../components/Footer/Footer";
 
 const RestaurantPage = () => {
-  const { states, sets } = useContext(GlobalStateContext)
+  const { states, sets } = useContext(GlobalStateContext);
   const pathParams = useParams();
-  console.log("pathParamsn", pathParams)
   const [foods, isLoadingFoods, errorFoods] = useRequestData(
     `${BASE_URL}/restaurants/${pathParams.id}`
   );
   // useProtectedPage();
 
-const AddProductToCart = () => {
-  sets.setOpenModal(true)
-}
+  const AddProductToCart = () => {
+    sets.setOpenModal(true);
+  };
 
-const onClickClose = () => sets.setOpenModal(false)
+  const addCart = (
+    idProduct,
+    nameProduct,
+    photoUrlProduct,
+    descriptionProduct,
+    priceProduct
+  ) => {
+    console.log(
+      "addCartFunction",
+      idProduct,
+      nameProduct,
+      photoUrlProduct,
+      descriptionProduct,
+      priceProduct
+    );
+  };
+
+  const onClickClose = () => sets.setOpenModal(false);
 
   const listFoods =
     foods &&
     foods.restaurant &&
-    foods.restaurant.products.map((food) => {
-      return (
-        <CardFood
-          key={food.id}
-          title={food.name}
-          image={food.photoUrl}
-          description={food.description}
-          price={food.price}
-          onClickAddProduct={AddProductToCart }
-        />
-      );
-    });
+    foods.restaurant.products.map(
+      ({ id, name, photoUrl, description, price }) => {
+        return (
+            <CardFood
+              key={id}
+              title={name}
+              image={photoUrl}
+              description={description}
+              price={price}
+              onClickAdd={AddProductToCart}
+
+              value={states.openModal}
+              onClickClose={onClickClose}
+            />
+        );
+      }
+    );
   return (
     <div>
       <h1>Restaurantes</h1>
 
-<ModalQuantityFood value={states.openModal} onClickClose={onClickClose} />
-
-      {foods && foods.restaurant && (<div>
-          
-          <img src={foods.restaurant.logoUrl} style={{width: '180px'}}/>
+      {foods && foods.restaurant && (
+        <div>
+          <img src={foods.restaurant.logoUrl} style={{ width: "180px" }} />
           <h1>{foods.restaurant.name}</h1>
           <p>{foods.restaurant.category}</p>
-          <p>{foods.restaurant.deliveryTime} min  Frete R$ {foods.restaurant.shipping}</p>
+          <p>
+            {foods.restaurant.deliveryTime} min Frete R${" "}
+            {foods.restaurant.shipping}
+          </p>
           <p>{foods.restaurant.address}</p>
-      </div>
+        </div>
       )}
 
       {isLoadingFoods && <p>Carregando</p>}
@@ -61,7 +83,7 @@ const onClickClose = () => sets.setOpenModal(false)
       {!isLoadingFoods && foods && listFoods.length === 0 && (
         <p>Não há nenhuma postagem</p>
       )}
-     <Footer />
+      <Footer />
     </div>
   );
 };
