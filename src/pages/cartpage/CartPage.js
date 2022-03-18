a58b4db2af3b3fddd385d37effd7b5f3cce6801b
Raquelmms <raqueldemedeirosmartins@gmail.com>
2022-03-18 15:00:28 -0300

    endereço do restaurante aparecendo no cart

--- src/pages/cartpage/CartPage.js
+++ src/pages/cartpage/CartPage.js
@@ -1,52 +1,75 @@
-import React from 'react';
-import axios from 'axios';
-import { BASE_URL } from '../../constants/urls';
-import { useEffect, useState, useContext, createContext } from 'react';
-import Footer from '../../components/Footer/Footer';
-import Header from '../../components/Header/Header';
-import { useNavigate } from 'react-router-dom';
-import { AddressContainer, RestaurantContainer, AdressDelivery,
-    AdressContainer,
-    CartContainer,
-    Title,
-    ShippingText,
-    SubtotalPrice,
-    TotalPrice,
-    PaymentMethodText,
-    ProductContainer,
-    ProductImageContainer,
-    ProductInfoContainer,
-    QuantityContainer,
-    ProductInfo,
-    RemoveButtonContainer,
-    RemoveButton,
-    ButtonContainer,
-    PaymentContainer,
-    Quantity,
-    CheckBox,} from './styled';
+import React from "react";
+import axios from "axios";
+import { BASE_URL } from "../../constants/urls";
+import { useEffect, useState, useContext, createContext } from "react";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import { useNavigate } from "react-router-dom";
 import {
-    getProfile, getDetailRestaurant,
-  } from "../../axiosRequests/user";
+  AddressContainer,
+  RestaurantContainer,
+  AdressDelivery,
+  AdressContainer,
+  CartContainer,
+  Title,
+  ShippingText,
+  SubtotalPrice,
+  TotalPrice,
+  PaymentMethodText,
+  ProductContainer,
+  ProductImageContainer,
+  ProductInfoContainer,
+  QuantityContainer,
+  ProductInfo,
+  RemoveButtonContainer,
+  RemoveButton,
+  ButtonContainer,
+  PaymentContainer,
+  Quantity,
+  CheckBox,
+} from "./styled";
+import { getProfile, getDetailRestaurant } from "../../axiosRequests/user";
 import { GlobalStateContext } from "../../components/Global/GlobalStateContext";
 import {
-    FormControl,
-    FormControlLabel,
-    Radio,
-    RadioGroup,
-  } from "@material-ui/core";
-  import CardFood from '../../components/CardFood/CardFood';
-  import { Button, Checkbox } from '@material-ui/core';
-import { RestaurantMenu } from '@material-ui/icons';
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+} from "@material-ui/core";
+import CardFood from "../../components/CardFood/CardFood";
+import { Button, Checkbox } from "@material-ui/core";
+import { RestaurantMenu } from "@material-ui/icons";
+import { useRequestData } from "../../hooks/useRequestData";
 
 const CartPage = () => {
-    const [address, setAddress] = useState({});
-    const { states, sets } = useContext(GlobalStateContext);
-    const [totalPrice, setTotalPrice] = useState(0);
-    const [product, setProduct] = useState([]);
+  const [address, setAddress] = useState({});
+  const { states, sets } = useContext(GlobalStateContext);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [product, setProduct] = useState([]);
   const [payment, setPayment] = useState("");
   const [shipping, setShipping] = useState([]);
   const navigate = useNavigate();
   const [restaurant, setRestaurant] = useState([]);
+  const [
+    restaurantDetails,
+    isLoadingRestaurantDetails,
+    errorRestaurantDetails,
+  ] = useRequestData(`${BASE_URL}/restaurants/${states.restaurantId}`);
+
+  console.log("DETALHES DO RESTAURANTE", restaurantDetails);
+
+//   const restaurantInfoList =
+//     states.restaurantId &&
+//     restaurantDetails &&
+//     restaurantDetails.restaurant.map((restautantInfo) => {
+//       return (
+//         <RestaurantContainer>
+//           <p>{restautantInfo.name}</p>
+//           <span>Restaurant Address</span>
+//           <span>Delivery time</span>
+//         </RestaurantContainer>
+//       );
+//     });
 
   const removeItem = (item) => {
     const newArray = product.filter((pdt) => {
@@ -55,107 +78,126 @@ const CartPage = () => {
     setProduct(newArray);
   };
 
-    useEffect(() => {
-        getProfile(setAddress);
-        getDetailRestaurant(states.cart.idRestaurant, setRestaurant);
-      }, []);
+  useEffect(() => {
+    getProfile(setAddress);
+    getDetailRestaurant(states.cart.idRestaurant, setRestaurant);
+  }, []);
 
-const renderAddress =  address.address ? (
+  const renderAddress = address.address ? (
     <AddressContainer>
-            <p>Endereço de entrega:</p>
-            <span>{address.address}</span>
-          </AddressContainer>
-    ) : (
+      <p>Endereço de entrega:</p>
+      <span>{address.address}</span>
+    </AddressContainer>
+  ) : (
     <AdressContainer>
-        <p>Favor cadastrar um endereçoclicando <strong onClick={() => navigate("/endereco")}>aqui.</strong></p>
+      <p>
+        Favor cadastrar um endereçoclicando{" "}
+        <strong onClick={() => navigate("/endereco")}>aqui.</strong>
+      </p>
     </AdressContainer>
-    );
+  );
 
-
-    return (
-        console.log("CARRINHO", states.cart),
-        console.log("RESTAURANTE", restaurant),
-        <>
+  return (
+    console.log("CARRINHO", states.cart),
+    console.log("RESTAURANTE", restaurant),
+    (
+      <>
         <Header />
         <CartContainer>
-            {renderAddress}
+          {renderAddress}
 
-            {restaurant.map((restaurant) => (
-            
+          {restaurantDetails && restaurantDetails.restaurant && (
+          <RestaurantContainer>
+        <p>{restaurantDetails.restaurant.name}</p>
+        <span>{restaurantDetails.restaurant.address}</span>
+        <span>{restaurantDetails.restaurant.deliveryTime}</span>
+    </RestaurantContainer>)}
+
+          {restaurant.map((restaurant) => (
             <RestaurantContainer>
-                <p>{restaurant.name}</p>
-                <span>Restaurant Address</span>
-                <span>Delivery time</span>
+              <p>{restaurant.name}</p>
+              <span>Restaurant Address</span>
+              <span>Delivery time</span>
             </RestaurantContainer>
-            ))}
- 
-        
-                {states.cart.map((item) => {
-                    return (
-                        <ProductContainer>
-                            <ProductImageContainer>
-                                <img src={item.photoUrl} alt="product Img" />
-                            </ProductImageContainer>
-                            <ProductInfoContainer>
-                                <QuantityContainer>
-                                    <Quantity>{item.quantity}</Quantity>
-                                </QuantityContainer>
-                                <ProductInfo>
-                                <p>{item.name}</p>
-                                <span>{item.description}</span>
-                                <strong>R${item.price}</strong>
-                                </ProductInfo>
-                            <RemoveButtonContainer>
-                                <RemoveButton onClick={() => removeItem(item)}>
-                                remover
-                                </RemoveButton>
-                            </RemoveButtonContainer>
-                            
-                            </ProductInfoContainer>
-                            </ProductContainer>
-                    );
-                }
-                )}
+          ))}
 
-                <ShippingText>
-                <p>Frete:</p>
-                <p>{shipping}</p>
-            </ShippingText>
-            
-            <SubtotalPrice>
-                <p>Subtotal:</p>
-                <p>{states.cart.reduce((frete, pdt) => {
-                    return pdt.price * pdt.quantity + frete;
-                }
-                , 0)}</p>
-            </SubtotalPrice>
-        
+          {states.cart.map((item) => {
+            return (
+              <ProductContainer>
+                <ProductImageContainer>
+                  <img src={item.photoUrl} alt="product Img" />
+                </ProductImageContainer>
+                <ProductInfoContainer>
+                  <QuantityContainer>
+                    <Quantity>{item.quantity}</Quantity>
+                  </QuantityContainer>
+                  <ProductInfo>
+                    <p>{item.name}</p>
+                    <span>{item.description}</span>
+                    <strong>R${item.price}</strong>
+                  </ProductInfo>
+                  <RemoveButtonContainer>
+                    <RemoveButton onClick={() => removeItem(item)}>
+                      remover
+                    </RemoveButton>
+                  </RemoveButtonContainer>
+                </ProductInfoContainer>
+              </ProductContainer>
+            );
+          })}
 
-            
-            <PaymentContainer>
+          <ShippingText>
+            <p>Frete:</p>
+            <p>{shipping}</p>
+          </ShippingText>
+
+          <SubtotalPrice>
+            <p>Subtotal:</p>
+            <p>
+              {states.cart.reduce((frete, pdt) => {
+                return pdt.price * pdt.quantity + frete;
+              }, 0)}
+            </p>
+          </SubtotalPrice>
+
+          <PaymentContainer>
             <p>Forma de pagamento</p>
             <FormControl component="fieldset">
-                <RadioGroup aria-label="payment" name="payment" value={payment} onChange={(e) => setPayment(e.target.value)}>
-                    <FormControlLabel value="dinheiro" control={<Radio />} label="Dinheiro" />
-                    <FormControlLabel value="cartao" control={<Radio />} label="Cartão" />
-                </RadioGroup>
+              <RadioGroup
+                aria-label="payment"
+                name="payment"
+                value={payment}
+                onChange={(e) => setPayment(e.target.value)}
+              >
+                <FormControlLabel
+                  value="dinheiro"
+                  control={<Radio />}
+                  label="Dinheiro"
+                />
+                <FormControlLabel
+                  value="cartao"
+                  control={<Radio />}
+                  label="Cartão"
+                />
+              </RadioGroup>
             </FormControl>
-            </PaymentContainer>
-            
-            
-            <ButtonContainer>
-                <Button variant="contained" color="primary" fullWidth style={{margin: "10px"}} >
-                    Confirmar
-                </Button>
-            </ButtonContainer>
-            
+          </PaymentContainer>
 
-           
-
+          <ButtonContainer>
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              style={{ margin: "10px" }}
+            >
+              Confirmar
+            </Button>
+          </ButtonContainer>
         </CartContainer>
         <Footer />
-        </>
-    );
+      </>
+    )
+  );
 };
 
 export default CartPage;
